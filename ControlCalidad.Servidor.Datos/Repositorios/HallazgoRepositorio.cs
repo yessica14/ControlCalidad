fbd966043017b4554f8c1208ca1ede140b5196bf
yessica14 <yessica.gimenez14@gmail.com>
2021-02-18 01:02:40 -0300

    se agregaron OP en el servidor

---
+++ ControlCalidad.Servidor.Datos/Repositorios/HallazgoRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using ControlCalidad.Servidor.Datos.Persistencia;
+using ControlCalidad.Servidor.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlCalidad.Servidor.Datos.Repositorios
+{
+    public class HallazgoRepositorio
+    {
+        public void CargarHallazgo()
+        {
+            var empresa = Singleton.getInstancia().ObtenerDatosDeEmpresa();
+            
+        }
+
+    }
+}
---
+++ ControlCalidad.Servidor.Datos/Repositorios/TurnoRepositorio.cs
@@ -0,0 +1,50 @@
+﻿using ControlCalidad.Servidor.Datos.Persistencia;
+using ControlCalidad.Servidor.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlCalidad.Servidor.Datos.Repositorios
+{
+    class TurnoRepositorio
+    {
+        public void CargarTurnos()
+        {
+            var empresa = Singleton.getInstancia().ObtenerDatosDeEmpresa();
+
+            empresa.ListaDeTurnos.Add
+            (
+                new Turno()
+                {
+                    Codigo = 100,
+                    nombre = "Mañana",
+                    HoraInicio = new TimeSpan(6, 0, 0),
+                    HoraFin = new TimeSpan(12, 59, 59)
+                }
+            );
+            empresa.ListaDeTurnos.Add
+           (
+               new Turno()
+               {
+                   Codigo = 101,
+                   nombre = "Tarde",
+                   HoraInicio = new TimeSpan(13, 0, 0),
+                   HoraFin = new TimeSpan(18, 59, 59)
+               }
+           );
+            empresa.ListaDeTurnos.Add
+           (
+               new Turno()
+               {
+                   Codigo = 102,
+                   nombre = "Noche",
+                   HoraInicio = new TimeSpan(19, 0, 0),
+                   HoraFin = new TimeSpan(23, 59, 59)
+               }
+           );
+
+        }
+    }
+}
